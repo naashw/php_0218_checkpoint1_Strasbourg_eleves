e6543e22520963c1807f3d0f695b63a81bb4e3a6
Luc Huet <luc.huet@wildcodeschool.fr>
2018-03-29 11:42:24 +0200

    commit Clement 2

--- public/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-//identifiant de connection à la basse de donnée à reconfigurer
-define("DSN", "mysql:host=localhost;dbname=checkpoint1");
-define("USER", "checkpoint1");
-define("PASS", "checkpoint1");
--- public/src/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function fullname($lastname,$firstname)
-{
-    return strtoupper($lastname)." ".ucwords(strtolower($firstname));
-}
