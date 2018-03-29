43e7dfe0929f608116b47b0532d99d5689a3698b
cpailler <36536967+cpailler@users.noreply.github.com>
2018-03-29 11:27:28 +0200

    documentation connect.php (#2)

--- public/connect.php
+++ public/connect.php
@@ -1,4 +1,5 @@
 <?php
+//identifiant de connection à la basse de donnée à reconfigurer
 define("DSN", "mysql:host=localhost;dbname=checkpoint1");
 define("USER", "checkpoint1");
 define("PASS", "checkpoint1");
